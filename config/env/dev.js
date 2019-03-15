7d7adf7a9a9e835874702e16fd21157ef65e6924
yu_ss <shuangshuang.yu@edaibu.net>
2019-03-15 09:59:57 +0800

    11

--- config/env/dev.js
+++ config/env/dev.js
@@ -4,5 +4,5 @@
  */
 module.exports = {
     port: 8888,
-    mongoURI: 'mongo://127.0.0.1:27017/YANGOU'
+    mongoURL: 'mongodb://127.0.0.1:27017/YANGOU'
 };
--- schema/UserSchema.js
+++ schema/UserSchema.js
@@ -1,3 +1,17 @@
 /**
+ * 用户Schema
  * Created by yu_ss on 2019-03-14 14:38
  */
+const mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+    username: String,   //用户名
+    password: String,
+    role: Number,   //用户角色
+    level: Number,   //等级
+    balance: Number, //账户余额
+    accumulate: Number //充值累计金额
+});
+
+let UserModel = mongoose.model('User', userSchema);
+module.exports = UserModel;
